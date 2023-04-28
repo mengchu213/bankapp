2ce3f143425e23ab274bb7c4254765a74c64dee5
Jose Infante <joseinfa1022@gmail.com>
2023-04-28 20:53:00 +0800

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
-import { useState } from 'react'
-import './App.css'
+import styles from "./style";
+import {
+  Navbar,
+  Hero,
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testimonials,
+  Clients,
+  CTA,
+  Footer,
+} from "./components";
 
-function App() {
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
 
-  return (
-    <div className='w-screen h-screen bg-blue-400'></div>
-  )
-}
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
 
-export default App
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats /> <Business /> <Billing /> <CardDeal /> <Testimonials />{" "}
+        <Clients /> <CTA /> <Footer />
+      </div>
+    </div>
+  </div>
+);
+
+export default App;
