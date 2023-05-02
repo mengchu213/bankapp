d9cf4f78b7fb51cb5502cb79120e2fd45356bf8a
Jose Infante <joseinfa1022@gmail.com>
2023-05-02 17:49:21 +0800

    ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,5 @@
 import styles from "./style";
-import {
-  Navbar,
-  Hero,
-  Stats,
-  Business,
-  Billing,
-  CardDeal,
-  Testimonials,
-  Clients,
-  CTA,
-  Footer,
-} from "./components";
+import {Navbar, Hero, CTA, Footer} from "./components";
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
@@ -28,8 +17,7 @@ const App = () => (
 
     <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
-        <Stats /> <Business /> <Billing /> <CardDeal /> <Testimonials />{" "}
-        <Clients /> <CTA /> <Footer />
+        <CTA /> <Footer />
       </div>
     </div>
   </div>
